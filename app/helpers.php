b285bbca46d2d45151a1ab223da34f0b13592e32
Daniel Campos <daniel.campos@dotlib.com.br>
2015-08-02 22:44:46 -0300

    Acicionando libs, novas migrations, update de views, selective e pickadate libs. Adicionando helpers.

--- app/helpers.php
+++ app/helpers.php
@@ -1,8 +1,8 @@
 <?php
 
 /**
- * Return sizes readable by humans
- */
+* Return sizes readable by humans
+*/
 function human_filesize($bytes, $decimals = 2)
 {
     $size = ['B', 'kB', 'MB', 'GB', 'TB', 'PB'];
@@ -13,9 +13,35 @@ function human_filesize($bytes, $decimals = 2)
 }
 
 /**
- * Is the mime type an image
- */
+* Is the mime type an image
+*/
 function is_image($mimeType)
 {
     return starts_with($mimeType, 'image/');
+}
+
+
+/**
+* Return "checked" if true
+*/
+function checked($value)
+{
+    return $value ? 'checked' : '';
+}
+
+
+/**
+* Return img url for headers
+*/
+function page_image($value = null)
+{
+    if (empty($value)) {
+        $value = config('blog.page_image');
+    }
+
+    if (! starts_with($value, 'http') && $value[0] !== '/') {
+        $value = config('blog.uploads.webpath') . '/' . $value;
+    }
+
+    return $value;
 }
