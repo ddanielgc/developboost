98515e8e03b890dee1614116bb17bee45da7c049
Daniel Campos <daniel.campos@dotlib.com.br>
2015-08-02 21:41:37 -0300

    Creating the upload dirs and files service. With their views controllers and formRequest filters. Adding S3 integration via composer and adding doctrine.

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -55,9 +55,19 @@ $router->group([
     resource('admin/post', 'PostController');
     resource('admin/tag', 'TagController', ['except' => 'show']);
     get('admin/upload', 'UploadController@index');
+
+    // upload
+    post('admin/upload/file', 'UploadController@uploadFile');
+    delete('admin/upload/file', 'UploadController@deleteFile');
+    post('admin/upload/folder', 'UploadController@createFolder');
+    delete('admin/upload/folder', 'UploadController@deleteFolder');
+
 });
 
 // Logging in and out
 get('/auth/login', 'Auth\AuthController@getLogin');
 post('/auth/login', 'Auth\AuthController@postLogin');
 get('/auth/logout', 'Auth\AuthController@getLogout');
+
+
+
--- config/blog.php
+++ config/blog.php
@@ -6,11 +6,25 @@
  * Time: 1:55 PM
  */
 
+
+/*
 return [
     'title' => 'Dev Boost',
     'posts_per_page' => 5,
     'uploads' => [
+
         'storage' => 'local',
         'webpath' => '/uploads',
+    ],
+];
+*/
+
+
+return [
+    'title' > 'Dev Boost',
+    'posts_per_page' => 5,
+    'uploads' => [
+        'storage' => 's3',
+        'webpath' => 'developboost.s3-website-us-east-1.amazonaws.com'
     ],
 ];
